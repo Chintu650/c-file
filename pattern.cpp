2351e86feab9d9abc3a969f3322a9c46764fcbcd
Chintu650 <subhasourabhsahu143@gmail.com>
2025-04-20 23:21:07 +0530

    Fixed

--- pattern.cpp
+++ pattern.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     int n;
+    cout << "Enter n : ";
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
@@ -10,5 +11,6 @@ int main(){
             }
         }
     cout<<endl;
-}
+    }
+return 0;
 }  
